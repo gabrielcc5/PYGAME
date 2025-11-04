5940482a4f2935ccab78e65f6d5b19014c711fed
Ana Clara <anacws1@al.insper.edu.br>
2025-11-04 15:31:13 -0300

    classe início

--- classes.py
+++ classes.py
@@ -1,13 +1,6 @@
 import pygame
+
 # Tela de início
-class StartScreen:
-    def __init__(self, screen, clock, width, height, fps, image_path):
-        self.screen = screen
-        self.clock = clock
-        self.width = width
-        self.height = height
-        self.fps = fps
-        
-        self.image = pygame.image.load("C:\Users\anacl\OneDrive\Documents\PyGame\PYGAME\Assets\Imagens\Tela de início.png")
-        self.image = pygame.transform.scale(self.image, (width, height))
-        self.img_rect = self.image.get_rect(center=(width // 2, height // 2))
+class StartScreen: 
+    def __init__(self, width, height, image_path="Assets/Imagens/Tela de início.png"):
+        self.image = pygame.image.load(image_path).convert()
