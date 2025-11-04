e37ba22ac0d9591bbfec254514dc19e99ca8d61b
Ana Clara <anacws1@al.insper.edu.br>
2025-11-04 15:30:26 -0300

    Inicializando

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+import pygame
+from classes import *
+from math import * 
+
+pygame.init()
+
+WIDTH, HEIGHT = 1200, 900
+FPS = 100
+
+# Configuração da tela 
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Spike Storm")
+clock = pygame.time.Clock()
+
+# Imagem de início
+start_image = pygame.image.load("Assets/Imagens/Tela de início.png").convert()
+
+
+
+running = True
+while running:
+    screen.blit(start_image, (0, 0))
+
+    clock.tick(FPS)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip() 
+
+pygame.quit()
