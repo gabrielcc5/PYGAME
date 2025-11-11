f3f69518da463d1e1169a0a243917a9c3c4c20ab
Gabriel Correa <gabrielcaccorrea@gmail.com>
2025-11-11 20:21:19 -0300

    musica de fundo e som de pulo

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import os
 
 pygame.init()
 pygame.mixer.init()
-pygame.mixer.music.load("Assets/Som /videoplayback.mp3")
-jump_sound = pygame.mixer.Sound("Assets/Som /Jump.wav")
+pygame.mixer.music.load("PYGAME/Assets/Som/videoplayback.mp3")
+jump_sound = pygame.mixer.Sound("PYGAME/Assets/Som/Jump.wav")
 jump_sound.set_volume(3.20)
 
 # Sistema de recorde
